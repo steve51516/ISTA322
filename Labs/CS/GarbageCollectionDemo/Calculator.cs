062aab2a310b6ae89f1514fcb216462556f20947
Steven Fairchild <steve51516@gmail.com>
2020-08-18 12:45:00 -0400

    added GC lab

---
+++ Labs/CS/GarbageCollectionDemo/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GarbageCollectionDemo
+{
+    class Calculator : IDisposable
+    {
+        private bool disposed = false;
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+
+        public void Dispose()
+        {
+            //Console.WriteLine("In dispose method, Calculator being disposed.");
+            if (!this.disposed)
+            {
+                Console.WriteLine("Calculator being disposed");
+            }
+            this.disposed = true;
+            GC.SuppressFinalize(this);
+        }
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+            Dispose();
+        }
+    }
+}
---
+++ Labs/CS/GarbageCollectionDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GarbageCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the garbage collection demo lab");
+            //Calculator calc = new Calculator();
+            //double result = calc.Divide(120, 15);
+            //Console.WriteLine($"120 / 15 = {result}");
+            Console.WriteLine("===========================================================");
+            using (Calculator calculator = new Calculator())
+            {
+                Console.WriteLine($"120 / 0 = {calculator.Divide(120, 0)}");
+            }
+            Console.WriteLine("Program finishing");
+        }
+    }
+}
