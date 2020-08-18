1382530eca1d55abf0f72b292f5060b55785455c
Steven Fairchild <steve51516@gmail.com>
2020-08-18 15:22:56 -0400

    Added Quiz07

---
+++ Quizs/Q07/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Q07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is CSharp Quiz07.");
+            IFirearm shotgun = new Shotgun();
+            shotgun.Shoot();
+
+            IFirearm rifle = new Rifle();
+            rifle.Shoot();
+
+            IFirearm pistol = new Pistol();
+            pistol.Shoot();
+        }
+    }
+    interface IFirearm
+    {
+        void Shoot();
+    }
+    public class Shotgun : IFirearm
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("I am a Shotgun and I go boom!");
+        }
+    }
+    public class Rifle : IFirearm
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("I am a Rifle and I go pow!");
+        }
+    }
+    public class Pistol : IFirearm
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("I am a Pistol and I go crack!");
+        }
+    }
+}
